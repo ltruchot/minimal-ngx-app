de2ee484235138fc99258a9faeef5683b394ff37
Loïc TRUCHOT <loic;truchot.pro@gmail.com>
2018-09-09 22:33:55 +0200

    feat(start-btn): add every example

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
 
+interface IAppInfos {
+  version: number;
+  theme: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Quizz generator';
+  isStarted = false;
+  appInfos: IAppInfos;
+  user: string;
+  private score = 0;
+
+  constructor() {
+    setTimeout(() => {
+      this.appInfos = {
+        version: 1,
+        theme: 'herbalism',
+      };
+    }, 2);
+  }
+
+  startGame() {
+    this.isStarted = true;
+  }
+
+  endGame() {
+    window.alert(`Game over ${this.user}. Score: ${this.score}`);
+    this.isStarted = false;
+  }
+}
